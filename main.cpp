c94a46bc7ff3e8f3021d0430b95c4ce0d32f4901
kirkhellsten <60529153+kirkhellsten@users.noreply.github.com>
2020-10-09 17:24:12 -0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+#include <vector>
+#include <map>
+using namespace std;
+
+map<int, bool> pf_generate_odd_non_prime_map(int pMaxOddNumberN) {
+
+    map<int, bool> retNonPrimeOddNumberMap;
+    int iFirstOddNumber = 3;
+    int iSpaceBetweenOddNumber = 2;
+
+    for (int c = iFirstOddNumber; c < 3+(2*(pMaxOddNumberN-1)); c += iSpaceBetweenOddNumber) {
+        retNonPrimeOddNumberMap[c] = false;
+    }
+
+    int iNonPrimeOdd = 9;
+    int iStartingNonPrimeOdd = 9;
+    int iInitialDiff = 6;
+    int iDiff = 6;
+    int iDiffIncrement = 4;
+
+    do {
+
+        if (retNonPrimeOddNumberMap.count(iNonPrimeOdd) == 0) {
+            iStartingNonPrimeOdd += iInitialDiff;
+            iNonPrimeOdd = iStartingNonPrimeOdd;
+            iDiff += iDiffIncrement;
+        } else {
+            retNonPrimeOddNumberMap[iNonPrimeOdd] = true;
+            iNonPrimeOdd += iDiff;
+        }
+
+    } while(retNonPrimeOddNumberMap.count(iStartingNonPrimeOdd) != 0);
+
+    return retNonPrimeOddNumberMap;
+}
+
+vector<int> pf_generate_odd_prime_list(int pMaxOddNumberN) {
+
+    map<int, bool> oddNonPrimeMap = pf_generate_odd_non_prime_map(pMaxOddNumberN);
+    vector<int> retPrimeList;
+
+    for(map<int, bool>::iterator it = oddNonPrimeMap.begin(); it != oddNonPrimeMap.end(); ++it) {
+        if (it->second == 0) {
+            retPrimeList.insert(retPrimeList.end(), it->first);
+        }
+    }
+
+    return retPrimeList;
+}
+
+int main()
+{
+    map<int, bool> nonPrimeOddMap = pf_generate_odd_non_prime_map(100);
+    for(map<int, bool>::iterator it = nonPrimeOddMap.begin(); it != nonPrimeOddMap.end(); ++it) {
+        cout << it->first << " " << (it->second ? "Not Prime" : "Prime") << endl;
+    }
+    vector<int> primeList = pf_generate_odd_prime_list(10000);
+
+    cout << "Prime Number List: " << endl;
+    for (int i = 0; i < primeList.size(); ++i) {
+        if (i < primeList.size() - 1)
+            cout << primeList[i] << ",";
+        else
+            cout << primeList[i];
+    }
+
+    return 0;
+}
